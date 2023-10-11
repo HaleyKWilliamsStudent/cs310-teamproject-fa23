b40bc7e66239d6df05fc0a178f4cc6206396959c
Kaylin Taylor <ktaylor20@stu.jsu.edu>
2023-10-11 12:07:24 -0500

    Finish Department Model Class

---
+++ src/edu/jsu/mcis/cs310/tas_fa23/Department.java
@@ -0,0 +1,26 @@
+
+package edu.jsu.mcis.cs310.tas_fa23;
+
+public class Department {
+    private int numericid;
+    private int terminalid;
+    private char description;
+}
+
+public int getNumericid{
+    return numericid;
+}
+
+public int getTerminalid{
+    return terminalid
+}
+  
+
+public char geDescription{
+    return description
+}
+
+@Override
+public String toString(){
+    return printOriginal
+}
