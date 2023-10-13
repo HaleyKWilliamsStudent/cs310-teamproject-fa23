302c64b4062d6766f444fd1a5263a3b46c9cf4ba
Kaylin Taylor <ktaylor20@stu.jsu.edu>
2023-10-13 00:11:04 -0500

    Finish Department

--- src/edu/jsu/mcis/cs310/tas_fa23/Department.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Department.java
@@ -2,25 +2,31 @@
 package edu.jsu.mcis.cs310.tas_fa23;
 
 public class Department {
-    private int numericid;
-    private int terminalid;
-    private char description;
-}
-
-public int getNumericid{
+    private final int numericid;
+    private final int terminalid;
+    private final char description;
+    
+    public Department(int numericid, int terminalid, char description){
+    this.numericid = numericid;
+    this.terminalid = terminalid;
+    this.description = description;
+    }
+    
+    public int getNumericid(){
     return numericid;
-}
+    }
 
-public int getTerminalid{
-    return terminalid
-}
+public int getTerminalid(){
+    return terminalid;
+    }
   
 
-public char geDescription{
-    return description
-}
-
+public char geDescription(){
+    return description;
+    }
 @Override
 public String toString(){
-    return printOriginal
+        String printOriginal = null;
+   return printOriginal;
+}
 }
