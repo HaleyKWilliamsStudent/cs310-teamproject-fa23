fec41d30b149093d42cbceab8ebdec934d31f669
Kaylin Taylor <ktaylor20@stu.jsu.edu>
2023-10-13 00:12:03 -0500

    Finish DepartmentDAO and added DepartmentDAO into DAOFactory

---
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DepartmentDAO.java
@@ -0,0 +1,64 @@
+
+package edu.jsu.mcis.cs310.tas_fa23.dao;
+
+import edu.jsu.mcis.cs310.tas_fa23.Department;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DepartmentDAO {
+    private static final String QUERY_FIND = "SELECT * FROM department WHERE id = ?";
+    private final DAOFactory daoFactory;
+    
+   
+    
+    DepartmentDAO (DAOFactory daoFactory){
+        this.daoFactory = daoFactory;
+    }
+    
+    public Department find(int numericid){
+       Department department = null;
+       PreparedStatement ps = null;
+       ResultSet rs = null;
+       
+       try{
+           Connection conn = daoFactory.getConnection();
+           
+           if(conn.isValid(0)){
+              ps = conn.prepareStatement(QUERY_FIND);
+              
+              ps.setInt(1,numericid);
+              
+              boolean hasresults = ps.execute();
+              
+              if(hasresults){
+                  rs = ps.getResultSet();
+                  
+                  while (rs.next()){
+                      int numericId = rs.getInt("numericid");
+                      
+                  }
+              }
+           }
+       } catch(SQLException e){
+           throw new DAOException(e.getMessage());
+       } finally{
+           if (rs != null) {
+               try {
+                   rs.close();
+               } catch(SQLException e){
+                   throw new DAOException(e.getMessage());
+               }
+           } 
+           if(ps != null){
+               try{
+                   ps.close();
+               } catch (SQLException e){
+                   throw new DAOException(e.getMessage());
+               }
+           }
+       }
+       return department;
+    }
+}
