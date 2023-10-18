004e597d1ec817e2e9ac4bb5934d7d29ef823376
unknown <wahickssware84@gmail.com>
2023-10-18 10:50:04 -0500

    Updates made to both utility methods in the DAOUtility class. Subject to change.

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
@@ -14,7 +14,32 @@ import com.github.cliftonlabs.json_simple.*;
  *
  */
 public final class DAOUtility {
+public static String getPunchListAsJSON(ArrayList<Punch> punchList) {
 
+        String dateFormat = "E MM/dd/yyyy HH:mm:ss";
+
+        ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
+
+        for (int i = 0; i < punchList.size(); i++) {
+            HashMap<String, String> punchData = new HashMap<>();
+            Punch punch = punchList.get(i);
+
+            punchData.put("badgeid", punch.getBadge().getId());
+            punchData.put("terminalid", String.valueOf(punch.getTerminalId()));
+            punchData.put("id", String.valueOf(punch.getId()));
+            punchData.put("punchtype", punch.getPunchType().toString());
+            punchData.put("adjustmenttype", punch.getAdjustmentType().toString());
+
+            String date = DateTimeFormatter.ofPattern(dateFormat).format(punch.getOriginalTimestamp()).toUpperCase();
+            punchData.put("originaltimestamp", date);
+
+            String adjustedTime = DateTimeFormatter.ofPattern(dateFormat).format(punch.getAdjustedTimestamp()).toUpperCase();
+            punchData.put("adjustedtimestamp", adjustedTime);
+
+            jsonData.add(punchData);
+        }
+        return Jsoner.serialize(jsonData);
+    }
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
         int totalMinutes = 0;
         // ** UTILITY METHOD WILL GO HERE *** - William H.
