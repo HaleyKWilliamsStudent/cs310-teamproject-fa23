b71e92e38e20e1f6fa8bf3f8b49270c2bdfdb47d
Kaylin Taylor <ktaylor20@stu.jsu.edu>
2023-10-19 23:02:47 -0500

    Utility Method to JSON Done

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
@@ -2,6 +2,8 @@ package edu.jsu.mcis.cs310.tas_fa23.dao;
 
 import java.time.*;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
@@ -14,5 +16,45 @@ import com.github.cliftonlabs.json_simple.*;
  * 
  */
 public final class DAOUtility {
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+        ArrayList<HashMap<string, string="">> jsonData;
+        
+        /* HashMap*/
+        HashMap<string, string=""> punchData= new HashMap<>;
+        
+        HashMap<string, string=""> punchData= new HashMap<>;
+        
+        HashMap<string, string=""> punchData= new HashMap<>;
+        
+        HashMap<string, string=""> punchData= new HashMap<>;
+        
+        HashMap<string, string""> punchData= new HashMap<>;
+        
+        /* Punch Data*/
+        punchData.put("id", String.valueof(punch.getId()));
+        
+        punchData.put("terminalid", String.valueof(punch.getTerminalid()));
+        
+        punchData.put("badgeid", String.valueof(punch.getBadgeid()));
+        
+        punchData.put("punchtype", punchType.toString());
+        
+        punchData.put("adjustmenttype", adjustmenttype.toString());
+        
+        punchData.put("adjustedtimestamp", adjustmentTimestamp.printAdjusted());
+        
+        punchData.put("originaltimestamp", originalTimestamp.printOriginal());
+        
+        /* Add HashMap to ArrayList*/
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        jsonData.add(punchData);
+        
+        String json = Jsoner.serialize (jsonData);
+    }
 
 }
