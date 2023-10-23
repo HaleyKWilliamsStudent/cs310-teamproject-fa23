fe1a8c4549261e0958ef8d11eed7ea9df61e5e70
wahickssware84 <wahickssware84@gmail.com>
2023-10-23 11:56:15 -0500

    Added signature for calculateTotalMinutes() method in the DAOUtility class.

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
@@ -18,31 +18,37 @@ import com.github.cliftonlabs.json_simple.*;
 public final class DAOUtility {
 
     private static Object jsonData;
-    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
         ArrayList<HashMap<String, String>> jsonData;
-        
-        /* HashMap*/
-        HashMap<String, String>punchData = new HashMap<>();
-        
-        /* Punch Data*/
+
+        /* HashMap */
+        HashMap<String, String> punchData = new HashMap<>();
+
+        /* Punch Data */
         punchData.put("id", String.valueof(punch.getId()));
-        
+
         punchData.put("terminalid", String.valueof(punch.getTerminalid()));
-        
+
         punchData.put("badgeid", String.valueof(punch.getBadgeid()));
-        
+
         punchData.put("punchtype", punchType.toString());
-        
+
         punchData.put("adjustmenttype", adjustmenttype.toString());
-        
+
         punchData.put("adjustedtimestamp", adjustmentTimestamp.printAdjusted());
-        
+
         punchData.put("originaltimestamp", originalTimestamp.printOriginal());
-        
-        /* Add HashMap to ArrayList*/
+
+        /* Add HashMap to ArrayList */
         jsonData.add(punchData);
- 
-        
-        String json = Jsoner.serialize (jsonData);
+
+        String json = Jsoner.serialize(jsonData);
+    }
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
+        int totalMinutes = 0;
+        /* *** This is the utility method for the backlog item "Add Utility Method to Calculate Total Minutes" in Canvas.
+        i.e., my code goes here. - William H. */
+        return int totalMinutes;
     }
 }
